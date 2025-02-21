fedead0415dbf556580f2c53c4955b8139b2752d
SprayingCactus <roccoagiacomino@gmail.com>
2025-02-20 19:24:12 -0600

    Update MoonHub.lua

--- MoonHub.lua
+++ MoonHub.lua
@@ -2,3 +2,4 @@ local Players = game:GetService("Players")
 local client = Players.LocalPlayer
 setclipboard("https://dsc.gg/getmoonexec")
 client:Kick("Moon is currently down. Please join discord for updates! dsc.gg/getmoonexec (copied to clipboard)")
+--if you see this, moon hub is switching loaders on next upd.
